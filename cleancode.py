38855dbfe80d4c589c80e0baca426c6bb80d3ca7
abdurrehman.nadeem <abdurrehman.nadeem@venturedive.com>
2019-05-13 14:29:53 +0500

    code snippet added

---
+++ cleancode.py
@@ -0,0 +1,15 @@
+def long_function(var_1, var_2, var_3, var_4, var_5, var_6):
+    print(var_1)
+
+
+# Aligned with opening delimiter.
+def long_function(var_1, var_2, var_3,
+                  var_4, var_5, var_6):
+    print(var_1)
+
+
+# Add 4 spaces (an extra level of indentation) to distinguish arguments from the rest.
+def long_function(
+        var_1, var_2, var_3,
+        var_4, var_5, var_6):
+    print(var_1)
